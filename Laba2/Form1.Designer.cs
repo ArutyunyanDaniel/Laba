60c2b14ab5e6b2e73c145ec377a5116a949605cb
Daniel <arutyunyan.daniel23@mail.ru>
2017-12-02 00:58:03 +0300

    Add Data Grid View for display information about DB

--- Laba2/Form1.Designer.cs
+++ Laba2/Form1.Designer.cs
@@ -28,10 +28,275 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            this.dataGridRoute = new System.Windows.Forms.DataGridView();
+            this.dataGridType = new System.Windows.Forms.DataGridView();
+            this.dataGridDimension = new System.Windows.Forms.DataGridView();
+            this.dataGridWeight = new System.Windows.Forms.DataGridView();
+            this.dataGridBrand = new System.Windows.Forms.DataGridView();
+            this.dataGridTransport = new System.Windows.Forms.DataGridView();
+            this.dataGridDriver = new System.Windows.Forms.DataGridView();
+            this.dataGridExperience = new System.Windows.Forms.DataGridView();
+            this.tabControlTableDB = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.tabPage6 = new System.Windows.Forms.TabPage();
+            this.tabPage7 = new System.Windows.Forms.TabPage();
+            this.tabPage8 = new System.Windows.Forms.TabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRoute)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridType)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDimension)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBrand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTransport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDriver)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridExperience)).BeginInit();
+            this.tabControlTableDB.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabPage5.SuspendLayout();
+            this.tabPage6.SuspendLayout();
+            this.tabPage7.SuspendLayout();
+            this.tabPage8.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridRoute
+            // 
+            this.dataGridRoute.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridRoute.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridRoute.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridRoute.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridRoute.Location = new System.Drawing.Point(3, 3);
+            this.dataGridRoute.Name = "dataGridRoute";
+            this.dataGridRoute.Size = new System.Drawing.Size(436, 142);
+            this.dataGridRoute.TabIndex = 0;
+            // 
+            // dataGridType
+            // 
+            this.dataGridType.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridType.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridType.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridType.Location = new System.Drawing.Point(0, 0);
+            this.dataGridType.Name = "dataGridType";
+            this.dataGridType.Size = new System.Drawing.Size(442, 148);
+            this.dataGridType.TabIndex = 1;
+            // 
+            // dataGridDimension
+            // 
+            this.dataGridDimension.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridDimension.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridDimension.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridDimension.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridDimension.Location = new System.Drawing.Point(0, 0);
+            this.dataGridDimension.Name = "dataGridDimension";
+            this.dataGridDimension.Size = new System.Drawing.Size(442, 148);
+            this.dataGridDimension.TabIndex = 2;
+            // 
+            // dataGridWeight
+            // 
+            this.dataGridWeight.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridWeight.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridWeight.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridWeight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridWeight.Location = new System.Drawing.Point(0, 0);
+            this.dataGridWeight.Name = "dataGridWeight";
+            this.dataGridWeight.Size = new System.Drawing.Size(442, 148);
+            this.dataGridWeight.TabIndex = 3;
+            // 
+            // dataGridBrand
+            // 
+            this.dataGridBrand.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridBrand.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridBrand.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridBrand.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridBrand.Location = new System.Drawing.Point(0, 0);
+            this.dataGridBrand.Name = "dataGridBrand";
+            this.dataGridBrand.Size = new System.Drawing.Size(442, 148);
+            this.dataGridBrand.TabIndex = 4;
+            // 
+            // dataGridTransport
+            // 
+            this.dataGridTransport.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridTransport.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridTransport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTransport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridTransport.Location = new System.Drawing.Point(0, 0);
+            this.dataGridTransport.Name = "dataGridTransport";
+            this.dataGridTransport.Size = new System.Drawing.Size(442, 148);
+            this.dataGridTransport.TabIndex = 5;
+            // 
+            // dataGridDriver
+            // 
+            this.dataGridDriver.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridDriver.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridDriver.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridDriver.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridDriver.Location = new System.Drawing.Point(3, 3);
+            this.dataGridDriver.Name = "dataGridDriver";
+            this.dataGridDriver.Size = new System.Drawing.Size(436, 142);
+            this.dataGridDriver.TabIndex = 6;
+            // 
+            // dataGridExperience
+            // 
+            this.dataGridExperience.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridExperience.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridExperience.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridExperience.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridExperience.Location = new System.Drawing.Point(0, 0);
+            this.dataGridExperience.Name = "dataGridExperience";
+            this.dataGridExperience.Size = new System.Drawing.Size(442, 148);
+            this.dataGridExperience.TabIndex = 7;
+            // 
+            // tabControlTableDB
+            // 
+            this.tabControlTableDB.Controls.Add(this.tabPage1);
+            this.tabControlTableDB.Controls.Add(this.tabPage2);
+            this.tabControlTableDB.Controls.Add(this.tabPage3);
+            this.tabControlTableDB.Controls.Add(this.tabPage4);
+            this.tabControlTableDB.Controls.Add(this.tabPage5);
+            this.tabControlTableDB.Controls.Add(this.tabPage6);
+            this.tabControlTableDB.Controls.Add(this.tabPage7);
+            this.tabControlTableDB.Controls.Add(this.tabPage8);
+            this.tabControlTableDB.Location = new System.Drawing.Point(12, 336);
+            this.tabControlTableDB.Name = "tabControlTableDB";
+            this.tabControlTableDB.SelectedIndex = 0;
+            this.tabControlTableDB.Size = new System.Drawing.Size(450, 174);
+            this.tabControlTableDB.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridRoute);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(442, 148);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Route";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridDriver);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(442, 148);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Driver";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridExperience);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(442, 148);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Expenciev";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.dataGridTransport);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(442, 148);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Transport";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.Controls.Add(this.dataGridDimension);
+            this.tabPage5.Location = new System.Drawing.Point(4, 22);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(442, 148);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Dimension";
+            this.tabPage5.UseVisualStyleBackColor = true;
+            // 
+            // tabPage6
+            // 
+            this.tabPage6.Controls.Add(this.dataGridType);
+            this.tabPage6.Location = new System.Drawing.Point(4, 22);
+            this.tabPage6.Name = "tabPage6";
+            this.tabPage6.Size = new System.Drawing.Size(442, 148);
+            this.tabPage6.TabIndex = 5;
+            this.tabPage6.Text = "Type";
+            this.tabPage6.UseVisualStyleBackColor = true;
+            // 
+            // tabPage7
+            // 
+            this.tabPage7.Controls.Add(this.dataGridWeight);
+            this.tabPage7.Location = new System.Drawing.Point(4, 22);
+            this.tabPage7.Name = "tabPage7";
+            this.tabPage7.Size = new System.Drawing.Size(442, 148);
+            this.tabPage7.TabIndex = 6;
+            this.tabPage7.Text = "Weight";
+            this.tabPage7.UseVisualStyleBackColor = true;
+            // 
+            // tabPage8
+            // 
+            this.tabPage8.Controls.Add(this.dataGridBrand);
+            this.tabPage8.Location = new System.Drawing.Point(4, 22);
+            this.tabPage8.Name = "tabPage8";
+            this.tabPage8.Size = new System.Drawing.Size(442, 148);
+            this.tabPage8.TabIndex = 7;
+            this.tabPage8.Text = "Brand";
+            this.tabPage8.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(935, 522);
+            this.Controls.Add(this.tabControlTableDB);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridRoute)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridType)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDimension)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridBrand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTransport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridDriver)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridExperience)).EndInit();
+            this.tabControlTableDB.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage5.ResumeLayout(false);
+            this.tabPage6.ResumeLayout(false);
+            this.tabPage7.ResumeLayout(false);
+            this.tabPage8.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
+        private System.Windows.Forms.DataGridView dataGridRoute;
+        private System.Windows.Forms.DataGridView dataGridType;
+        private System.Windows.Forms.DataGridView dataGridDimension;
+        private System.Windows.Forms.DataGridView dataGridWeight;
+        private System.Windows.Forms.DataGridView dataGridBrand;
+        private System.Windows.Forms.DataGridView dataGridTransport;
+        private System.Windows.Forms.DataGridView dataGridDriver;
+        private System.Windows.Forms.DataGridView dataGridExperience;
+        private System.Windows.Forms.TabControl tabControlTableDB;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.TabPage tabPage6;
+        private System.Windows.Forms.TabPage tabPage7;
+        private System.Windows.Forms.TabPage tabPage8;
 
         #endregion
     }
--- Laba2/Form1.cs
+++ Laba2/Form1.cs
@@ -12,9 +12,49 @@ namespace Laba2
 {
     public partial class Form1 : Form
     {
+        private MyModel m_dbContext;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dbContext = new MyModel();
+            DisplayTableInForm();
+        }
+
+        private void DisplayTableInForm()
+        {
+            dataGridBrand.DataSource = m_dbContext.Brands.ToList();
+            dataGridBrand.Columns[2].Visible = false;
+
+            dataGridDimension.DataSource = m_dbContext.Dimensions.ToList();
+            dataGridDimension.Columns[4].Visible = false;
+
+            dataGridDriver.DataSource = m_dbContext.Drivers.ToList();
+            dataGridDriver.Columns[5].Visible = false;
+            dataGridDriver.Columns[6].Visible = false;
+            dataGridDriver.Columns[7].Visible = false;
+
+            dataGridTransport.DataSource = m_dbContext.Transports.ToList();
+            dataGridTransport.Columns[6].Visible = false;
+            dataGridTransport.Columns[7].Visible = false;
+            dataGridTransport.Columns[8].Visible = false;
+            dataGridTransport.Columns[9].Visible = false;
+            dataGridTransport.Columns[10].Visible = false;
+
+
+            dataGridWeight.DataSource = m_dbContext.Weights.ToList();
+            dataGridWeight.Columns[2].Visible = false;
+
+            dataGridType.DataSource = m_dbContext.Types.ToList();
+            dataGridType.Columns[2].Visible = false;
+
+            dataGridExperience.DataSource = m_dbContext.Experiences.ToList();
+            dataGridExperience.Columns[2].Visible = false;
+
+            dataGridRoute.DataSource = m_dbContext.Routes.ToList();
         }
     }
 }
